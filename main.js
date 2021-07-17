3a211d2b5d4622c75ee4a6de93866fff46da5dbb
SHREYAAS1000 <75013816+SHREYAAS1000@users.noreply.github.com>
2021-07-17 13:29:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 function setup()
 {
-    canvas=createCanvas(400,400);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
 
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+poseNet = ml5.poseNet(video, modeLoaded);
+
+poseNet.on("pose",gotposes)
+
+}
+
+function draw()
+{
+    background("#0000FF")
+}
+
+function modeLoaded()
+{
+    console.log("poseNet is Loaded")
+}
+
+function gotposes(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+    }
 }
